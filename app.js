6bf32561aa05c58123b81fe2843431d0b3ff2243
AndreyKapus <kapustnikov@ukr.net>
2023-11-13 13:09:06 +0200

    midlewars

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const fs = require("fs").promises;
+const moment = require("moment");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+
+app.use(async (req, res, next) => {
+  const { method, url } = req;
+  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+  await fs.appendFile("server.log", `\n${method} ${url} ${date}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
