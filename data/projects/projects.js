46dbb73bd9703f4caa37f8375854f15e7fec8123
AndreyKapus <kapustnikov@ukr.net>
2023-11-13 15:32:05 +0200

    find by id

--- data/projects/projects.js
+++ data/projects/projects.js
@@ -1,5 +1,6 @@
 const projects = [
   {
+    id: "32423",
     name: "Petly",
     techs:
       "HTML, SASS, React, Redux, Node.JS, Axios, responsive design, REST Api, Webpack",
@@ -7,6 +8,7 @@ const projects = [
   },
 
   {
+    id: "76653",
     name: "Filmoteka",
     techs: "HTML, SASS, responsive design, JS, Axios, REST Api, parcel",
     descr: "Application to create your own movie library. Team project",
--- routes/api/projects.js
+++ routes/api/projects.js
@@ -12,4 +12,25 @@ router.get("/", (req, res) => {
     },
   });
 });
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const result = projects.find((item) => item.id === id);
+  if (!result) {
+    res.status(404).json({
+      status: "error",
+      code: 404,
+      message: `project with id: ${id} not found`,
+    });
+  }
+
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
+});
+
 module.exports = router;
