ac75cd414e6c6588faf57188c3745b8f687b8621
AndreyKapus <kapustnikov@ukr.net>
2023-11-14 15:02:06 +0200

    add + update

--- data/projects/index.js
+++ data/projects/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { all } = require("../../routes/api/projects");
+const { v4 } = require("uuid");
 
 const projectsPath = path.join(__dirname, "projects.json");
 
@@ -13,10 +14,35 @@ const getAll = async () => {
 const getById = async (id) => {
   const allProjects = await getAll();
   const result = allProjects.find((item) => item.id === id);
-  return result;
+  return result || null;
+};
+
+const addProject = async (data) => {
+  const allProjects = await getAll();
+  const newProject = {
+    id: v4(),
+    ...data,
+  };
+
+  allProjects.push(newProject);
+  await fs.writeFile(projectsPath, JSON.stringify(allProjects, null, 2));
+  return newProject;
+};
+
+const updateById = async (id, data) => {
+  const allProjects = await getAll();
+  const index = allProjects.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+
+  allProjects[index] = { id, ...data };
+  await fs.writeFile(projectsPath, JSON.stringify(allProjects, null, 2));
+  return allProjects[index];
 };
 
 module.exports = {
   getAll,
   getById,
+  addProject,
 };
