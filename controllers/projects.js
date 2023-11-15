0be81a2a86c9d37ac0a4aee1b6ea3d79664d6140
AndreyKapus <kapustnikov@ukr.net>
2023-11-15 12:28:56 +0200

    schemas

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,109 +1,82 @@
 const projects = require("../models/projects");
 const { v4 } = require("uuid");
-const { HttpError } = require("../helpers");
-const Joi = require("joi");
-
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  techs: Joi.string().required(),
-  descr: Joi.string().required(),
-});
+const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res, next) => {
-  try {
-    const result = await projects.getAll();
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result: result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+  const result = await projects.getAll();
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: result,
+    },
+  });
 };
 
 const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await projects.getById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
+  const { id } = req.params;
+  const result = await projects.getById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 };
 
 const add = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await projects.addProject(req.body);
-    res.status(201).json({
-      status: "success",
-      code: 201,
-      data: {
-        result: result,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
+  //   const { error } = addSchema.validate(req.body);
+  //   if (error) {
+  //     throw HttpError(400, error.message);
+  //   }
+  const result = await projects.addProject(req.body);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      result: result,
+    },
+  });
 };
 
 const updateById = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { id } = req.params;
-    const result = await projects.updateById(id, req.body);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: {
-        result,
-      },
-    });
-  } catch (error) {
-    next(error);
+  //   const { error } = addSchema.validate(req.body);
+  //   if (error) {
+  //     throw HttpError(400, error.message);
+  //   }
+  const { id } = req.params;
+  const result = await projects.updateById(id, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 };
 
 const deleteById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await projects.deleteById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({
-      message: "Deleted",
-    });
-  } catch (error) {
-    next(error);
+  const { id } = req.params;
+  const result = await projects.deleteById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.json({
+    message: "Deleted",
+  });
 };
 
 module.exports = {
-  getAll,
-  getById,
-  add,
-  updateById,
-  deleteById,
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
 };
--- routes/api/projects.js
+++ routes/api/projects.js
@@ -2,14 +2,16 @@ const express = require("express");
 const ctrl = require("../../controllers/projects");
 
 const router = express.Router();
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/projects");
 
 router.get("/", ctrl.getAll);
 
 router.get("/:id", ctrl.getById);
 
-router.post("/", ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", ctrl.updateById);
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
 
 router.delete("/:id", ctrl.deleteById);
 
