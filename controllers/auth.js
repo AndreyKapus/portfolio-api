b6feb362202f8084984be63eb13dd3e873c1f49e
AndreyKapus <kapustnikov@ukr.net>
2023-11-28 14:16:51 +0200

    jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
 const bcrypt = require("bcrypt");
 const { User } = require("../models/users");
 const { HttpError, ctrlWrapper } = require("../helpers");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { password } = req.body;
@@ -12,6 +15,30 @@ const register = async (req, res) => {
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Wrong email or password");
+  }
+
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    throw HttpError(401, "Wrong email or password");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+
+  res.json({
+    token,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
