4a0ca8d364666c94502582c1f2ce7050869f79af
AndreyKapus <kapustnikov@ukr.net>
2023-12-11 11:55:37 +0200

    model fix

--- models/project.js
+++ models/project.js
@@ -3,10 +3,6 @@ const { handleMongooseError } = require("../helpers");
 
 const projectSchema = new Schema(
   {
-    img: {
-      type: String,
-      required: true,
-    },
     name: {
       type: String,
       required: true,
